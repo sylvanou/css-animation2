a80b05116e75df924522c25fb55229f1b42a9e7c
sylvanou <sylvanoumbac24@gmail.com>
2018-03-12 12:27:38 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,290 @@
+window.onscroll = function(e) {
+    if(document.body.scrollTop < 256 || document.documentElement.scrollTop < 256) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '0px 0px';
+    }
+    if(document.body.scrollTop > 256 || document.documentElement.scrollTop > 256) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-256px 0px';
+    }
+    if(document.body.scrollTop > 512 || document.documentElement.scrollTop > 512) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-512px 0px';
+    }
+    if(document.body.scrollTop > 768 || document.documentElement.scrollTop > 768) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-768px 0px';
+    }
+    if(document.body.scrollTop > 1024 || document.documentElement.scrollTop > 1024) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-1024px 0px';
+    }
+    if(document.body.scrollTop > 1280 || document.documentElement.scrollTop > 1280) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-1280px 0px';
+    }
+    if(document.body.scrollTop > 1536 || document.documentElement.scrollTop > 1536) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-1536px 0px';
+    }
+    if(document.body.scrollTop > 1792 || document.documentElement.scrollTop > 1792) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-1792px 0px';
+    }
+    if(document.body.scrollTop > 2048 || document.documentElement.scrollTop > 2048) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-2048px 0px';
+    }
+    if(document.body.scrollTop > 2304 || document.documentElement.scrollTop > 2304) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-2304px 0px';
+    }
+    if(document.body.scrollTop > 2560 || document.documentElement.scrollTop > 2560) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-2560px 0px';
+    }
+    if(document.body.scrollTop > 2816 || document.documentElement.scrollTop > 2816) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-2816px 0px';
+    }
+    if(document.body.scrollTop > 3072 || document.documentElement.scrollTop > 3072) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-3072px 0px';
+    }
+    if(document.body.scrollTop > 3328 || document.documentElement.scrollTop > 3328) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-3328px 0px';
+    }
+    if(document.body.scrollTop > 3584 || document.documentElement.scrollTop > 3584) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-3584px 0px';
+    }
+    if(document.body.scrollTop > 3840 || document.documentElement.scrollTop > 3840) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-3840px 0px';
+    }
+    if(document.body.scrollTop > 4096 || document.documentElement.scrollTop > 4096) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-4096px 0px';
+    }
+    if(document.body.scrollTop > 4352 || document.documentElement.scrollTop > 4352) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-4352px 0px';
+    }
+    if(document.body.scrollTop > 4608 || document.documentElement.scrollTop > 4608) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-4608px 0px';
+    }
+    if(document.body.scrollTop > 4864 || document.documentElement.scrollTop > 4864) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-4864px 0px';
+    }
+    if(document.body.scrollTop > 5120 || document.documentElement.scrollTop > 5120) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-5120px 0px';
+    }
+    if(document.body.scrollTop > 5376 || document.documentElement.scrollTop > 5376) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-5376px 0px';
+    }
+    if(document.body.scrollTop > 5632 || document.documentElement.scrollTop > 5632) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-5632px 0px';
+    }
+    if(document.body.scrollTop > 5888 || document.documentElement.scrollTop > 5888) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-5888px 0px';
+    }
+    if(document.body.scrollTop > 6144 || document.documentElement.scrollTop > 6144) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-6144px 0px';
+    }
+    if(document.body.scrollTop > 6400 || document.documentElement.scrollTop > 6400) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-6400px 0px';
+    }
+    if(document.body.scrollTop > 6656 || document.documentElement.scrollTop > 6656) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-6656px 0px';
+    }
+    if(document.body.scrollTop > 6912 || document.documentElement.scrollTop > 6912) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-6912px 0px';
+    }
+    if(document.body.scrollTop > 7168 || document.documentElement.scrollTop > 7168) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-7168px 0px';
+    }
+    if(document.body.scrollTop > 7424 || document.documentElement.scrollTop > 7424) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-7424px 0px';
+    }
+    if(document.body.scrollTop > 7680 || document.documentElement.scrollTop > 7680) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-7680px 0px';
+    }
+    if(document.body.scrollTop > 7936 || document.documentElement.scrollTop > 7936) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-7936px 0px';
+    }
+    if(document.body.scrollTop > 8192 || document.documentElement.scrollTop > 8192) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-8192px 0px';
+    }
+    if(document.body.scrollTop > 8448 || document.documentElement.scrollTop > 8448) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-8448px 0px';
+    }
+    if(document.body.scrollTop > 8704 || document.documentElement.scrollTop > 8704) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-8704px 0px';
+    }
+    if(document.body.scrollTop > 8960 || document.documentElement.scrollTop > 8960) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-8960px 0px';
+    }
+    if(document.body.scrollTop > 9216 || document.documentElement.scrollTop > 9216) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-9216px 0px';
+    }
+    if(document.body.scrollTop > 9472 || document.documentElement.scrollTop > 9472) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-9472px 0px';
+    }
+    if(document.body.scrollTop > 9728 || document.documentElement.scrollTop > 9728) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-9728px 0px';
+    }
+    if(document.body.scrollTop > 9984 || document.documentElement.scrollTop > 9984) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-9984px 0px';
+    }
+    if(document.body.scrollTop > 10240 || document.documentElement.scrollTop > 10240) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-10240px 0px';
+    }
+    if(document.body.scrollTop > 10496 || document.documentElement.scrollTop > 10496) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-10496px 0px';
+    }
+    if(document.body.scrollTop > 10752 || document.documentElement.scrollTop > 10752) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-10752px 0px';
+    }
+    if(document.body.scrollTop > 11008 || document.documentElement.scrollTop > 11008) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-11008px 0px';
+    }
+    if(document.body.scrollTop > 11264 || document.documentElement.scrollTop > 11264) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-11264px 0px';
+    }
+    if(document.body.scrollTop > 11520 || document.documentElement.scrollTop > 11520) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-11520px 0px';
+    }
+    if(document.body.scrollTop > 11776 || document.documentElement.scrollTop > 11776) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-11776px 0px';
+    }
+    if(document.body.scrollTop > 12032 || document.documentElement.scrollTop > 12032) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-12032px 0px';
+    }
+    if(document.body.scrollTop > 12288 || document.documentElement.scrollTop > 12288) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-12288px 0px';
+    }
+    if(document.body.scrollTop > 12554 || document.documentElement.scrollTop > 12554) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-12554px 0px';
+    }
+    if(document.body.scrollTop > 12800 || document.documentElement.scrollTop > 12800) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-12800px 0px';
+    }
+    if(document.body.scrollTop > 13056 || document.documentElement.scrollTop > 13056) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-13056px 0px';
+    }
+    if(document.body.scrollTop > 13312 || document.documentElement.scrollTop > 13312) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-13312px 0px';
+    }
+    if(document.body.scrollTop > 13568 || document.documentElement.scrollTop > 13568) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-13568px 0px';
+    }
+    if(document.body.scrollTop > 13824 || document.documentElement.scrollTop > 13824) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-13824px 0px';
+    }
+    if(document.body.scrollTop > 14080 || document.documentElement.scrollTop > 14080) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-14080px 0px';
+    }
+    if(document.body.scrollTop > 14336 || document.documentElement.scrollTop > 14336) {
+        console.log('Scrolled here', document.body.scrollTop, document.documentElement.scrollTop);
+
+        document.getElementById('dragon').style.backgroundPosition = '-14336px 0px';
+    }
+    else {
+
+    }
+}
